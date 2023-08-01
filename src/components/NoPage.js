dfbbd92dc99a7f6a2fef88b68eefc5f051f810e6
Lex <sicoealexandra97@gmail.com>
2023-08-01 14:35:35 +0300

    updated HomePage, NoPage and TrailList page

---
+++ src/components/NoPage.js
@@ -0,0 +1,30 @@
+import Grid from "@mui/joy/Grid";
+import Typography from "@mui/joy/Typography";
+import { Link } from "react-router-dom";
+import Button from "@mui/joy/Button";
+
+const NoPage = () => {
+  return (
+    <Grid
+      container
+      justifyContent="center"
+      direction="column"
+      alignItems="center"
+      height="100%"
+      style={{
+        backgroundColor: "white	",
+        width: "100vw",
+        height: "100vh",
+        fontWeight: "400",
+      }}
+    >
+      <Typography level="h1" textAlign="center" textColor="#FAA0A0">
+        Error 404. Page not found.
+      </Typography>
+      <Link to="/">
+        <Button sx={{ fontSize: "15px", marginTop: "20px" }}>Return</Button>
+      </Link>
+    </Grid>
+  );
+};
+export default NoPage;
---
+++ src/components/TrailList.js
@@ -0,0 +1,6 @@
+import { Box } from "@mui/joy";
+
+const TrailList = () => {
+  return <Box>Hello</Box>;
+};
+export default TrailList;

14008e530a1db9bdf946096cef11c4ba8c06c6b4
Lex <sicoealexandra97@gmail.com>
2023-08-05 08:44:46 +0300

    imported SwipeableDrawer component into Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,110 @@
-import { Grid, Typography, Button } from "@mui/joy";
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import { Button, Grid, Typography } from "@mui/joy";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import * as React from "react";
 import { Link } from "react-router-dom";
-
 const Navbar = () => {
-  return (
-    <Grid
-      xs={12}
-      container
-      justifyContent="space-between"
-      alignItems="center"
-      sx={{ backgroundColor: "black", p: 3 }}
-    >
-      <Link to={"/"}>
-        <Typography
-          textAlign="center"
-          textColor="#C7E8CA"
-          sx={{
-            fontSize: { xs: "25px" },
-          }}
-        >
-          Peak Pursuit
-        </Typography>
-      </Link>
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-      <Link to={"/saved-trails-list"}>
-        <Button size="md">See your saved trails</Button>
-      </Link>
-    </Grid>
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  return (
+    <>
+      <Grid
+        xs={12}
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        sx={{ backgroundColor: "black", p: 3 }}
+      >
+        <Link to={"/"}>
+          <Typography
+            textAlign="center"
+            textColor="#C7E8CA"
+            sx={{
+              fontSize: { xs: "25px" },
+            }}
+          >
+            Peak Pursuit
+          </Typography>
+        </Link>
+
+        <Link to={"/saved-trails-list"}>
+          <Button size="md">See your saved trails</Button>
+        </Link>
+        <div>
+          {["left", "right", "top", "bottom"].map((anchor) => (
+            <React.Fragment key={anchor}>
+              <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+              <SwipeableDrawer
+                anchor={anchor}
+                open={state[anchor]}
+                onClose={toggleDrawer(anchor, false)}
+                onOpen={toggleDrawer(anchor, true)}
+              >
+                {list(anchor)}
+              </SwipeableDrawer>
+            </React.Fragment>
+          ))}
+        </div>
+      </Grid>
+    </>
   );
 };
 export default Navbar;

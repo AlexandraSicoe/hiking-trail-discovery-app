a5f5e045a9e0c85b328302abe890747231e2c294
Lex <sicoealexandra97@gmail.com>
2023-08-05 20:56:20 +0300

    fixed carousel, fixed carousel buttons

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,37 +1,50 @@
 import React from "react";
-import Slider from "react-slick";
+import ReactSwipe from "react-swipe";
 import CarouselImg1 from "../images/c1.avif";
 import CarouselImg2 from "../images/c2.avif";
 import CarouselImg3 from "../images/c3.avif";
+import { Box, Button, Grid, Typography } from "@mui/joy";
 
-export default function SimpleSlider() {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+const Carousel = () => {
+  let reactSwipeEl;
+
   return (
-    <Slider {...settings}>
-      <div>
-        <img
-          style={{ height: "300px", width: "500px" }}
-          src={CarouselImg1}
-        ></img>
-      </div>
-      <div>
-        <img
-          style={{ height: "300px", width: "500px" }}
-          src={CarouselImg2}
-        ></img>
-      </div>
-      <div>
-        <img
-          style={{ height: "300px", width: "500px" }}
-          src={CarouselImg3}
-        ></img>
-      </div>
-    </Slider>
+    <Box>
+      <ReactSwipe
+        className="carousel"
+        swipeOptions={{ continuous: false }}
+        ref={(el) => (reactSwipeEl = el)}
+      >
+        <Box>
+          <img
+            style={{ height: "300px", width: "500px" }}
+            src={CarouselImg1}
+          ></img>
+        </Box>
+        <Box>
+          <img
+            style={{ height: "300px", width: "500px" }}
+            src={CarouselImg2}
+          ></img>
+        </Box>
+        <Box>
+          <img
+            style={{ height: "300px", width: "500px" }}
+            src={CarouselImg3}
+          ></img>
+        </Box>
+      </ReactSwipe>
+      <Box display="flex" justifyContent="center" sx={{ p: 2 }}>
+        <Button size="sm" sx={{ m: 1 }} onClick={() => reactSwipeEl.prev()}>
+          Previous
+        </Button>
+
+        <Button size="sm" sx={{ m: 1 }} onClick={() => reactSwipeEl.next()}>
+          Next
+        </Button>
+      </Box>
+    </Box>
   );
-}
+};
+
+export default Carousel;
